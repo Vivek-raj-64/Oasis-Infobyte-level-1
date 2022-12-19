16437ea641456ed668760b0b25e79f5ee9ecb5c2
Vivek Raj <92172556+Vivek-raj-64@users.noreply.github.com>
2022-12-19 13:31:38 +0530

    Add files via upload

---
+++ Task-2/jsfunctions.js
@@ -0,0 +1,20 @@
+console.log("This is javascript fnctions tutorial");
+
+function greet(name) {
+    console.log(name + " is a good car.");
+}
+
+let name = "Punto";
+let name1 = "Verna";
+let name2 = "Hexa";
+let name3 = "Brezza";
+
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
+
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, "2"));
